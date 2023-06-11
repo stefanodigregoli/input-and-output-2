233f59385d1cb8aa34c5c931d2bd1a32b529c618
ariosto22 <emanuele.digregoli40@gmail.com>
2023-06-11 15:56:50 +0200

    Input and output - 2 Create a class called PrintYourNameAndSurname Provide the class PrintYourNameAndSurname with a main method: public static void main(String[] args) { ... } Inside the main method write two lines of code that print on the same line your name followed by a space and by your surname The expected output should be this: YourName YourSurname

---
+++ src/PrintYourNameAndSurname.java
@@ -0,0 +1,9 @@
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+
+        String name = "Stefano ";
+        String surname = "Di Gregoli";
+        System.out.print(name);
+        System.out.print(surname);
+    }
+}
